94a14a9711638b70178fc3cfb37d053e093db48b
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-05-18 18:00:52 +1000

    add extra modules

--- src/app/modules/pool/pool.module.ts
+++ src/app/modules/pool/pool.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class IdModule { }
+export class PoolModule { }
