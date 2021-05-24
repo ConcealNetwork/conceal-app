a80b11a015c1c2bc1b0bcba93909761f64766d7e
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-05-24 13:40:23 +1000

    add dynamic page titles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-	 // You will want to hide and show a back-arrow depending on navigation state, if linking to other components from within the components
-  showBackArrow = false;
+	pageTitle: string = '';
 
-  constructor() { }
+  constructor(
+		private activatedRoute: ActivatedRoute,
+		private router: Router
+	) { }
 
-	toggleSidenav() {	}
+	private setPageTitle(): void {
+		this.router.events.pipe(
+			filter(event => event instanceof NavigationEnd),
+			map(() => {
+				let child = this.activatedRoute.firstChild;
+
+				if (!child) {
+					return this.activatedRoute.snapshot.data.title;
+				}
+
+				while (child.firstChild) {
+					child = child.firstChild;
+				}
+
+				if (child.snapshot.data.title) {
+					return child.snapshot.data.title;
+				}
+			})
+		).subscribe((title: string) => this.pageTitle = title);
+	}
 
   ngOnInit(): void {
-  }
+		this.setPageTitle();
+	}
 
 }
