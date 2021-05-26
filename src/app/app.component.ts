a6e9bc5e80ee09bc4ca0ffe76cb8ba2b4bdf9faa
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-05-26 15:05:28 +1000

    update mainfest and add maskable icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { getSupportedInputTypes, Platform, supportsPassiveEventListeners, supportsScrollBehavior } from '@angular/cdk/platform';
 
 @Component({
@@ -8,10 +9,27 @@ import { getSupportedInputTypes, Platform, supportsPassiveEventListeners, suppor
 })
 export class AppComponent {
 
+	// check platform properties
   supportedInputTypes = Array.from(getSupportedInputTypes()).join(', ');
   supportsPassiveEventListeners = supportsPassiveEventListeners();
   supportsScrollBehavior = supportsScrollBehavior();
+	isSmallScreen: boolean = false;
 
-  constructor(public platform: Platform) {}
+  constructor(
+		public breakpointObserver: BreakpointObserver,
+		public platform: Platform
+	) {	}
+
+	ngOnInit() {
+		this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
+		.subscribe((state: BreakpointState) => {
+			if (state.matches) {
+				// Matches small viewport or handset in portrait mode
+				this.isSmallScreen = true;
+			} else {
+				this.isSmallScreen = false;
+			}
+		});
+	}
 
 }
