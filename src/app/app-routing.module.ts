00f4d85adbed2001030d1ccd64172f013bc979aa
ThrownLemon <travis_winsor@hotmail.com>
2021-06-01 19:02:07 +1000

    clean up styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-	{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), data: { title: "Conceal Hub" } },
+	{ path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), data: { title: "Conceal Hub" } },
 	{ path: 'wallet', loadChildren: () => import('./modules/wallet/wallet.module').then(m => m.WalletModule), data: { title: "Conceal Wallet" } },
 	{ path: 'settings', loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule), data: { title: "Conceal Hub Settings" } },
 	{ path: 'pay', loadChildren: () => import('./modules/pay/pay.module').then(m => m.PayModule), data: { title: "Conceal Pay" } },
@@ -11,7 +11,7 @@ const routes: Routes = [
 	{ path: 'explorer', loadChildren: () => import('./modules/explorer/explorer.module').then(m => m.ExplorerModule), data: { title: "Conceal Explorer" } },
 	{ path: 'bridge', loadChildren: () => import('./modules/bridge/bridge.module').then(m => m.BridgeModule), data: { title: "Conceal Bridge" } },
 	{ path: 'deposits', loadChildren: () => import('./modules/deposits/deposits.module').then(m => m.DepositsModule), data: { title: "Conceal Deposits" } },
-	{ path: '**', redirectTo: 'home', pathMatch: 'full' }
+	{ path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
