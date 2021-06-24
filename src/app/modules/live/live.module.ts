4d8239dabbc849a00e08b344cd24dfae029e130b
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-06-24 13:58:18 +1000

    live proc

--- src/app/modules/live/live.module.ts
+++ src/app/modules/live/live.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 import { LiveRoutingModule } from './live-routing.module';
 import { LiveComponent } from './live.component';
 
@@ -11,7 +17,12 @@ import { LiveComponent } from './live.component';
   ],
   imports: [
     CommonModule,
-    LiveRoutingModule
+    LiveRoutingModule,
+		MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatGridListModule,
+		MatCardModule
   ]
 })
 export class LiveModule { }
