282b16264b8fbe80ec26e81de0b9ee2b56f70c5d
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-01 18:32:35 +1100

    add send funds form workflows

--- src/app/modules/wallet/dialogs/send/send.component.ts
+++ src/app/modules/wallet/dialogs/send/send.component.ts
@@ -14,6 +14,15 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
 export class SendDialog {
 
 	isSmallScreen: boolean = false;
+	transaction: boolean = false;
+	confirmed: boolean = false;
+
+	// form values
+	amount: number = 0;
+	fee: number = 0.001;
+	fromAddress: string = 'ccx7Nvoi2iYj7uw4k5EEGCN6ogBPzGrDMY6Lnj16ZwSKZrHZ5zVh29HXtznxBsofFP8JB32YYBmtwLdoEirjAbYo4DBZfA4r7X';
+
+	hasTwoFa: boolean = true;
 
 	constructor (
 		public breakpointObserver: BreakpointObserver,
@@ -27,40 +36,80 @@ export class SendDialog {
 			} else {
 				this.isSmallScreen = false;
 			}
-		});
+		})
+		if (this.hasTwoFa) {
+			this.formAuthorise.addControl('twofaFormControl', new FormControl('', [
+				Validators.minLength(6),
+				Validators.maxLength(6),
+				Validators.pattern('^[0-9]*$'),
+				Validators.required,
+			]))
+		}
+		if (!this.hasTwoFa) {
+			this.formAuthorise.addControl('passwordFormControl', new FormControl('', [
+				Validators.required,
+			]))
+		}
 	}
 
-	pay: FormGroup = new FormGroup({
-		amountFormControl: new FormControl('', [
+	formTransaction: FormGroup = new FormGroup({
+		amount: new FormControl('', [
 			Validators.pattern('^[0-9]+\.?[0-9]*$'),
 			Validators.required,
 		]),
-		toaddressFormControl: new FormControl('', [
+		toAddress: new FormControl('', [
 			Validators.required,
 			Validators.minLength(98),
 			Validators.maxLength(98),
 		]),
-		paymentidFormControl: new FormControl('', [
+		paymentID: new FormControl('', [
 			Validators.minLength(64),
 			Validators.maxLength(64),
 		]),
-		messageFormControl: new FormControl('', [
+		message: new FormControl('', [
 			Validators.minLength(1),
 			Validators.maxLength(260),
 		]),
 	});
 
+	formConfirm: FormGroup = new FormGroup({
+		amount: new FormControl({disabled: true}, Validators.required),
+		fee: new FormControl({disabled: true}, Validators.required),
+		fromAddress: new FormControl({disabled: true}, Validators.required),
+		toAddress: new FormControl({disabled: true}, Validators.required),
+		paymentID: new FormControl({disabled: true}),
+		message: new FormControl({disabled: true}),
+	});
+
+	formAuthorise: FormGroup = new FormGroup({});
+
+	setAmount(value:number) {}
+
 	close() {
 		this.dialogRef.close(true);
 	}
 
-	setAmount(value:number) {}
+	review() {
+		if (this.formTransaction.valid) {
+			this.formConfirm.controls.amount.patchValue(this.formTransaction.value.amount, { emitEvent: true });
+			this.formConfirm.controls.fee.patchValue(this.fee, { emitEvent: true });
+			this.formConfirm.controls.toAddress.patchValue(this.formTransaction.value.toAddress, { emitEvent: true });
+			this.formConfirm.controls.fromAddress.patchValue(this.fromAddress, { emitEvent: true });
+			this.formConfirm.controls.paymentID.patchValue(this.formTransaction.value.paymentID, { emitEvent: true });
+			this.formConfirm.controls.message.patchValue(this.formTransaction.value.message, { emitEvent: true });
+			this.transaction = true;
+		}
+	}
 
-	submit() {
-		if (this.pay.valid) {
-			console.log('valid');
-		} else {
-			console.log('not valid');
+	confirm() {
+		if (this.formConfirm.valid) {
+			this.confirmed = true;
+		}
+	}
+
+	authorise() {
+		if (this.formAuthorise.valid) {
+			this.dialogRef.close(true);
 		}
 	}
 
