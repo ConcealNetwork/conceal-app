0777c69edc82aa19a6d06a376cbfbab72c2ca6cb
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-07 22:15:36 +1100

    add app configs to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,5 @@ export const environment = {
   production: true,
   environment: 'PROD',
   walletAPI: 'https://api.wallet.conceal.network/api',
-	wsEndpoint: 'wss://supportccx.com',
-	RTCPeerConfiguration: {
-    iceServers: [
-      {
-        urls: 'stun:stun1.l.google.com:19302'
-      }
-    ]
-  }
+	defaultFee: 0.001000000,
 };
