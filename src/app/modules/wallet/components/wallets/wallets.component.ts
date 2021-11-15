b13e00c2caa7e37872e8fe00f844bfc2c80d80f8
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-15 22:04:39 +1100

    more wallet fns inside wallet cp and check wallet limit

--- src/app/modules/wallet/components/wallets/wallets.component.ts
+++ src/app/modules/wallet/components/wallets/wallets.component.ts
@@ -1,12 +1,18 @@
+// App Variables
+import { environment } from 'src/environments/environment';
+
+// Angular Core
 import { Component, OnInit, Input } from '@angular/core';
 import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
 // Services
+import { ThemingService } from 'src/app/shared/services/theming.service';
 import { HelperService } from 'src/app/shared/services/helper.service';
-import { DialogService } from 'src/app/shared/services/dialog.service';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { DataService } from 'src/app/shared/services/data.service';
-import { ThemingService } from 'src/app/shared/services/theming.service';
+import { CloudService } from 'src/app/shared/services/cloud.service';
+import { DialogService } from 'src/app/shared/services/dialog.service';
+import { SnackbarService } from 'src/app/shared/services/snackbar.service';
 
 export interface Wallets {
   item: any;
@@ -41,6 +47,9 @@ export class WalletsComponent implements OnInit {
 
 	@Input() wallets!: Wallets;
 
+	isLoading: boolean = false;
+	activeWallets: number = 0;
+	activeWalletsLimit: number = environment.walletLimit;
 	portfolioCCX: any = 0;
 	portfolioBTC: any = 0;
 	portfolioUSD: any = 0;
@@ -48,8 +57,10 @@ export class WalletsComponent implements OnInit {
   constructor (
 		private helperService: HelperService,
 		private dialogService: DialogService,
+		private snackbarService: SnackbarService,
 		private apiService: ApiService,
 		private dataService: DataService,
+		private cloudService: CloudService,
 		private themingService: ThemingService,
 	) {}
 
@@ -68,6 +79,8 @@ export class WalletsComponent implements OnInit {
   ngOnInit(): void {
 		this.dataService.wallets$.subscribe((wallets:any) => {
 			this.wallets = wallets;
+			this.activeWallets = Object.keys(this.wallets).length;
+			console.log(this.activeWallets);
 			this.portfolioCCX = Object.keys(wallets).reduce((acc, curr) => acc + wallets[curr].balance + wallets[curr].locked || acc, 0);
 			this.apiService.getPrice('btc').subscribe((price:any) => {
 				this.portfolioBTC = (price.conceal.btc * this.portfolioCCX);
@@ -77,5 +90,42 @@ export class WalletsComponent implements OnInit {
 			})
 		})
   }
+
+	deleteWallet(wallet:string, balance:number) {
+		this.isLoading = true;
+		if (balance < 1) {
+			this.cloudService.deleteWallet(wallet).subscribe((data:any) => {
+				if (data.result === 'success') {
+					this.snackbarService.openSnackBar('Wallet deleted', 'Dismiss');
+					this.cloudService.getWalletsData().subscribe((data:any) => {
+						this.dataService.setWallets(data.message.wallets);
+					})
+					this.isLoading = false;
+				} else {
+					this.snackbarService.openSnackBar(data.message, 'Dismiss');
+					this.isLoading = false;
+				}
+			})
+		} else {
+			this.snackbarService.openSnackBar('Wallet not empty: Remove funds first', 'Dismiss');
+			this.isLoading = false;
+		}
+	}
+
+	createWallet() {
+		this.isLoading = true;
+		this.cloudService.createWallet().subscribe((data:any) => {
+			if (data.result === 'success') {
+				this.snackbarService.openSnackBar('Wallet created', 'Dismiss');
+				this.cloudService.getWalletsData().subscribe((data:any) => {
+					this.dataService.setWallets(data.message.wallets);
+				})
+				this.isLoading = false;
+			} else {
+				this.snackbarService.openSnackBar(data.message, 'Dismiss');
+				this.isLoading = false;
+			}
+		})
+	}
 
 }
