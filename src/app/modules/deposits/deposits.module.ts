966fb20f886b27283c8a85a1e857ab59719b2ed2
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-18 04:20:32 +1100

    add basic layout for deposits

--- src/app/modules/deposits/deposits.module.ts
+++ src/app/modules/deposits/deposits.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CdkTableModule } from '@angular/cdk/table';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 
 import { DepositsRoutingModule } from './deposits-routing.module';
 import { DepositsComponent } from './deposits.component';
@@ -11,7 +22,19 @@ import { DepositsComponent } from './deposits.component';
   ],
   imports: [
     CommonModule,
-    DepositsRoutingModule
+    DepositsRoutingModule,
+		CommonModule,
+		FlexLayoutModule,
+		FormsModule,
+    ReactiveFormsModule,
+		MatAutocompleteModule,
+		MatButtonModule,
+		MatInputModule,
+		MatCardModule,
+		MatIconModule,
+		MatMenuModule,
+		MatTableModule,
+		CdkTableModule
   ]
 })
 export class DepositsModule { }
