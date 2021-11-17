82a2590842157fa246831e7888eb9f02fb9f9edc
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-18 03:25:36 +1100

    add addDeposit func

--- src/app/modules/deposits/deposits.component.ts
+++ src/app/modules/deposits/deposits.component.ts
@@ -1,3 +1,6 @@
+// App Variables
+import { environment } from 'src/environments/environment';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposits.component.scss']
 })
 export class DepositsComponent implements OnInit {
+
+	interestRates = environment.interestRates;
 
   constructor() { }
 
