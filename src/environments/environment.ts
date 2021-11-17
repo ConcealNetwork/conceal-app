19339dff175383cea31a753755d6b15f3efa68ab
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-18 03:06:54 +1100

    add InterestRates to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,21 @@ export const environment = {
   environment: 'DEV',
   walletAPI: 'https://api.wallet.conceal.network/api',
 	defaultFee: 0.001000,
-	walletLimit: 2
+	walletLimit: 2,
+	interestRates: [
+		[0.24, 0.33, 0.41],
+		[0.50, 0.67, 0.83],
+		[0.78, 1.03, 1.28],
+		[1.07, 1.40, 1.73],
+		[1.38, 1.79, 2.21],
+		[1.70, 2.20, 4.70],
+		[2.04, 2.63, 3.21],
+		[2.40, 3.07, 3.73],
+		[2.78, 3.53, 4.28],
+		[3.17, 4.00, 4.83],
+		[3.58, 4.49, 5.41],
+		[4.00, 5.00, 6.00],
+	]
 };
 
 /*
