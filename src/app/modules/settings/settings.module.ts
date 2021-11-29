11774aedd76590d74438a751679223f6ba734c91
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-11-29 16:00:50 +1100

    update settings module

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
@@ -11,7 +22,18 @@ import { SettingsComponent } from './settings.component';
   ],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatAutocompleteModule,
+		FlexLayoutModule,
+		MatInputModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		MatTooltipModule
   ]
 })
 export class SettingsModule { }
