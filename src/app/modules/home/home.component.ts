5fbc6a67ed71dc6ca0b7197014575491647f6e64
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-12-15 15:37:02 +1100

    update new/social page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -69,24 +69,91 @@ export class HomeComponent implements OnInit {
 	getArticles() {
 		let medium = this.apiService.getMediumArticles().subscribe((data:any) => {
 			if (data) {
-				this.posts.push(...data.items);
+				for (let i = 0; i < data.items.length; i++) {
+					// add an new object in array of objects
+					this.posts.push({
+						author: data.items[i].author,
+						title: data.items[i].title,
+						description: data.items[i].description,
+						pubDate: data.items[i].pubDate,
+						link: data.items[i].link,
+						thumbnail: data.items[i].thumbnail,
+						source: 'Medium'
+					});
+				}
 				this.isLoading = false;
 			} else {
 				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not retrieve articles', 'Dismiss');
+				this.snackbarService.openSnackBar('Could not retrieve medium articles', 'Dismiss');
 			}
 		})
-		// let twitter = this.apiService.getTwitterArticles().subscribe((data:any) => {
-		// 	if (data) {
-		// 		this.posts.push(...data.items);
-		// 		this.isLoading = false;
-		// 	} else {
-		// 		this.isLoading = false;
-		// 		this.snackbarService.openSnackBar('Could not retrieve articles', 'Dismiss');
-		// 	}
-		// })
+		let twitter = this.apiService.getTwitterArticles().subscribe((data:any) => {
+			if (data) {
+				for (let i = 0; i < data.items.length; i++) {
+					// add an new object in array of objects
+					this.posts.push({
+						author: data.items[i].author,
+						title: data.items[i].title,
+						description: data.items[i].description,
+						pubDate: data.items[i].pubDate,
+						link: data.items[i].link,
+						thumbnail: data.items[i].thumbnail,
+						source: 'Twitter'
+					});
+				}
+				this.isLoading = false;
+			} else {
+				this.isLoading = false;
+				this.snackbarService.openSnackBar('Could not retrieve twitter articles', 'Dismiss');
+			}
+		})
+		let youtube = this.apiService.getYouTubePosts().subscribe((data:any) => {
+			if (data) {
+				for (let i = 0; i < data.items.length; i++) {
+					// add an new object in array of objects
+					this.posts.push({
+						author: data.items[i].author,
+						title: data.items[i].title,
+						description: data.items[i].description,
+						pubDate: data.items[i].pubDate,
+						link: data.items[i].link,
+						thumbnail: data.items[i].thumbnail,
+						source: 'YouTube'
+					});
+				}
+				this.isLoading = false;
+			} else {
+				this.isLoading = false;
+				this.snackbarService.openSnackBar('Could not retrieve youtube articles', 'Dismiss');
+			}
+		})
+		let reddit = this.apiService.getRedditPosts().subscribe((data:any) => {
+			if (data) {
+				for (let i = 0; i < data.items.length; i++) {
+					// add an new object in array of objects
+					this.posts.push({
+						author: data.items[i].author,
+						title: data.items[i].title,
+						description: data.items[i].description,
+						pubDate: data.items[i].pubDate,
+						link: data.items[i].link,
+						thumbnail: data.items[i].thumbnail,
+						source: 'Reddit'
+					});
+				}
+				this.isLoading = false;
+			} else {
+				this.isLoading = false;
+				this.snackbarService.openSnackBar('Could not retrieve reddit articles', 'Dismiss');
+			}
+		})
 		// call wallets and deposits
-		Promise.all([medium]);
+		Promise.all([medium, twitter, reddit, youtube]).catch(err => {
+			if(err) {
+				this.isLoading = false;
+				this.snackbarService.openSnackBar('Could not all social data', 'Dismiss');
+			}
+		});
 	}
 
 }
