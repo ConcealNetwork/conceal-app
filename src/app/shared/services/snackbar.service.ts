23cf6c6d61b821b91f5831e7c5cbf03b251fc8fa
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2021-12-21 04:38:26 +1100

    fix for snackbar blocking navigation

--- src/app/shared/services/snackbar.service.ts
+++ src/app/shared/services/snackbar.service.ts
@@ -1,6 +1,7 @@
 // Angular Core
 import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, } from '@angular/material/snack-bar';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Injectable({
 	providedIn: 'root'
@@ -11,7 +12,38 @@ export class SnackbarService {
 	snackbarVerticalPosition: MatSnackBarVerticalPosition = 'bottom';
 	snackbarHorizontalPosition: MatSnackBarHorizontalPosition = 'right';
 
-	constructor(private _snackBar: MatSnackBar) { }
+	constructor (
+		private _snackBar: MatSnackBar,
+		public breakpointObserver: BreakpointObserver
+	) {
+		// watch for changes of the screen size
+		this.breakpointObserver.observe([
+			Breakpoints.XSmall,
+			Breakpoints.Small,
+			Breakpoints.Medium,
+			Breakpoints.Large,
+			Breakpoints.XLarge
+		]).subscribe((state: BreakpointState) => {
+			if (state.matches) {
+				if (state.breakpoints[Breakpoints.XSmall]) {
+					this.snackbarHorizontalPosition = 'center';
+					this.snackbarVerticalPosition = 'top';
+				} else if (state.breakpoints[Breakpoints.Small]) {
+					this.snackbarHorizontalPosition = 'center';
+					this.snackbarVerticalPosition = 'top';
+				} else if (state.breakpoints[Breakpoints.Medium]) {
+					this.snackbarHorizontalPosition = 'right';
+					this.snackbarVerticalPosition = 'bottom';
+				} else if (state.breakpoints[Breakpoints.Large]) {
+					this.snackbarHorizontalPosition = 'right';
+					this.snackbarVerticalPosition = 'bottom';
+				} else if (state.breakpoints[Breakpoints.XLarge]) {
+					this.snackbarHorizontalPosition = 'right';
+					this.snackbarVerticalPosition = 'bottom';
+				}
+			}
+		})
+	}
 
 	openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
@@ -19,7 +51,7 @@ export class SnackbarService {
 			panelClass: 'snack-notify',
 			horizontalPosition: this.snackbarHorizontalPosition,
 			verticalPosition: this.snackbarVerticalPosition,
-    });
+    })
   }
 
 }
