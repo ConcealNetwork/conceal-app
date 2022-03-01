2d83bcf84195d08f12a6274105102681cecaf88f
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2022-03-01 11:06:44 +1100

    bridge init

--- src/app/modules/bridge/bridge.module.ts
+++ src/app/modules/bridge/bridge.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared.module';
+import { MaterialModule } from 'src/app/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BridgeRoutingModule } from './bridge-routing.module';
 import { BridgeComponent } from './bridge.component';
+
+import { DataService } from './services/data.service';
 
 
 @NgModule({
@@ -11,7 +15,13 @@ import { BridgeComponent } from './bridge.component';
   ],
   imports: [
 		SharedModule,
-    BridgeRoutingModule
-  ]
+    BridgeRoutingModule,
+		MaterialModule,
+		FormsModule,
+		ReactiveFormsModule
+  ],
+	providers: [
+		DataService
+  ],
 })
 export class BridgeModule { }
