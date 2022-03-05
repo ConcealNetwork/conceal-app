e59d465366b0ac4a9ce51510c2e4611293652e36
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2022-03-05 15:48:47 +1100

    update ver no.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   environment: 'DEV',
-	version: '1.2.0',
+	version: '1.3.0',
   walletAPI: 'https://api.wallet.conceal.network/api',
   coinStats: 'https://api.coinstats.app',
   gecko: 'https://api.coingecko.com',
