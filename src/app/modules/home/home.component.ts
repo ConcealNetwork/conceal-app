03d9047cf0488647c89e96235c8df5b37ee13aa6
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2022-05-28 23:30:42 +1000

    swap news for home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,33 +2,18 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
-import * as moment from 'moment';
-
 // Services
-import { ApiService } from 'src/app/shared/services/api.service';
 import { ThemingService } from 'src/app/shared/services/theming.service';
-import { SnackbarService } from 'src/app/shared/services/snackbar.service';
-import { DialogService } from 'src/app/shared/services/dialog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 	animations: [
-		trigger('listAnimation1', [
-			transition('* => *', [ // each time the binding value changes
-				query(':enter', [
-					style({ opacity: 0 }),
-					stagger(100, [
-						animate('0.4s', style({ opacity: 1 }))
-					])
-				], {optional: true})
-			])
-		]),
-		trigger('listAnimation2', [
-			transition('* => *', [ // each time the binding value changes
-				query(':enter', [
-					style({ opacity: 0 }),
+		trigger('transition', [
+			transition(':enter', [
+				query('#cards', [
+					style({ opacity: 0}),
 					stagger(100, [
 						animate('0.4s', style({ opacity: 1 }))
 					])
@@ -41,13 +26,9 @@ export class HomeComponent implements OnInit {
 
 	// Variables
 	isLoading: boolean = true;
-	posts: any = [];
 
   constructor(
-		private apiService: ApiService,
 		private themingService: ThemingService,
-		private snackbarService: SnackbarService,
-		private dialogService: DialogService,
 	) { }
 
 	getThemingService() {
@@ -55,104 +36,7 @@ export class HomeComponent implements OnInit {
 	}
 
   ngOnInit(): void {
-		this.getArticles();
-	}
-
-	openArticle(item: string) {
-		this.dialogService.openArticleDialog(item);
-	}
-
-	toDate(time: number) {
-		return moment(time);
-	}
-
-	getArticles() {
-		let medium = this.apiService.getMediumArticles().subscribe((data:any) => {
-			if (data) {
-				for (let i = 0; i < data.items.length; i++) {
-					// add an new object in array of objects
-					this.posts.push({
-						author: data.items[i].author,
-						title: data.items[i].title,
-						description: data.items[i].description,
-						pubDate: data.items[i].pubDate,
-						link: data.items[i].link,
-						thumbnail: data.items[i].thumbnail,
-						source: 'Medium'
-					});
-				}
-				this.isLoading = false;
-			} else {
-				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not retrieve medium articles', 'Dismiss');
-			}
-		})
-		let twitter = this.apiService.getTwitterArticles().subscribe((data:any) => {
-			if (data) {
-				for (let i = 0; i < data.items.length; i++) {
-					// add an new object in array of objects
-					this.posts.push({
-						author: data.items[i].author,
-						title: data.items[i].title,
-						description: data.items[i].description,
-						pubDate: data.items[i].pubDate,
-						link: data.items[i].link,
-						thumbnail: data.items[i].thumbnail,
-						source: 'Twitter'
-					});
-				}
-				this.isLoading = false;
-			} else {
-				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not retrieve twitter articles', 'Dismiss');
-			}
-		})
-		let youtube = this.apiService.getYouTubePosts().subscribe((data:any) => {
-			if (data) {
-				for (let i = 0; i < data.items.length; i++) {
-					// add an new object in array of objects
-					this.posts.push({
-						author: data.items[i].author,
-						title: data.items[i].title,
-						description: data.items[i].description,
-						pubDate: data.items[i].pubDate,
-						link: data.items[i].link,
-						thumbnail: data.items[i].thumbnail,
-						source: 'YouTube'
-					});
-				}
-				this.isLoading = false;
-			} else {
-				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not retrieve youtube articles', 'Dismiss');
-			}
-		})
-		let reddit = this.apiService.getRedditPosts().subscribe((data:any) => {
-			if (data) {
-				for (let i = 0; i < data.items.length; i++) {
-					// add an new object in array of objects
-					this.posts.push({
-						author: data.items[i].author,
-						title: data.items[i].title,
-						description: data.items[i].description,
-						pubDate: data.items[i].pubDate,
-						link: data.items[i].link,
-						source: 'Reddit'
-					});
-				}
-				this.isLoading = false;
-			} else {
-				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not retrieve reddit articles', 'Dismiss');
-			}
-		})
-		// call wallets and deposits
-		Promise.all([medium, twitter, reddit, youtube]).catch(err => {
-			if(err) {
-				this.isLoading = false;
-				this.snackbarService.openSnackBar('Could not all social data', 'Dismiss');
-			}
-		});
+		this.isLoading = false;
 	}
 
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,10 @@ import { MaterialModule } from 'src/app/material.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { ArticleDialog } from './dialogs/article/article.dialog';
 
 @NgModule({
   declarations: [
-    HomeComponent,
-		ArticleDialog
+    HomeComponent
   ],
   imports: [
     HomeRoutingModule,
