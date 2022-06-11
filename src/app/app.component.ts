b04af11a5276cd07801f3d59efb5905ec15ea6e1
ThrownLemon <4219774+ThrownLemon@users.noreply.github.com>
2022-06-11 23:17:25 +1000

    add new animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,6 +40,49 @@ export class AppComponent implements OnInit, OnDestroy {
       this.className = theme;
       this.applyThemeOnOverlays();
     });
+
+		// Some random colors
+		const colors = ["#FFC55C", "#FFB52E", "#D18700", "#A36A00", "#754C00"];
+		const numBalls = 50;
+		const balls = [];
+
+		for (let i = 0; i < numBalls; i++) {
+			let ball = document.createElement("div");
+			ball.classList.add("ball");
+			ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+			ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+			ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+			ball.style.transform = `scale(${Math.random()})`;
+			ball.style.width = `${Math.random()}em`;
+			ball.style.height = ball.style.width;
+
+			balls.push(ball);
+			var element = document.getElementsByClassName('container');
+			element[0].prepend(ball);
+		}
+
+		// Keyframes
+		balls.forEach((el, i, ra) => {
+			let to = {
+				x: Math.random() * (i % 2 === 0 ? -11 : 11),
+				y: Math.random() * 12
+			};
+
+			let anim = el.animate(
+				[
+					{ transform: "translate(0, 0)" },
+					{ transform: `translate(${to.x}rem, ${to.y}rem)` }
+				],
+				{
+					duration: (Math.random() + 1) * 2000, // random duration
+					direction: "alternate",
+					fill: "both",
+					iterations: Infinity,
+					easing: "ease-in-out"
+				}
+			);
+		});
+
 	}
 
   /**
