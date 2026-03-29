be80543685b3628e1c3acab89d8c18b1041622ee
acktarius <xchaps@aol.com>
2026-03-29 15:35:00 -0400

    stabilize core to 18

--- src/app/modules/bridge/services/api.service.ts
+++ src/app/modules/bridge/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
